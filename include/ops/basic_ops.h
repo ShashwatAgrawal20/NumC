e612ddad7e2dd5c4cc9cbe57fcd6510589b2864b
Shashwat Agrawal <shashwatagrawal473@gmail.com>
2025-04-16 16:03:48 +0530

    chore: removed unwanted macro from the public api

--- include/ops/basic_ops.h
+++ include/ops/basic_ops.h
@@ -3,19 +3,6 @@
 
 #include "core/ndarray.h"
 
-/*
- * not sure if I want to use those nc_set and nc_get things??
- */
-#define NC_BINARY_OP(type, op)                       \
-    do {                                             \
-        type *ad = (type *)a->data;                  \
-        type *bd = (type *)b->data;                  \
-        type *rd = (type *)result->data;             \
-        for (size_t i = 0; i < a->total_size; ++i) { \
-            rd[i] = ad[i] op bd[i];                  \
-        }                                            \
-    } while (0)
-
 ndarray_t *nc_add(ndarray_t *a, ndarray_t *b);
 ndarray_t *nc_sub(ndarray_t *a, ndarray_t *b);
 ndarray_t *nc_mul(ndarray_t *a, ndarray_t *b);
